4c881f47a10dc5c2d52e0881c6ca8388e5a33a5f
windows <veeranki2014@gmail.com>
2026-01-25 23:30:11 -0600

    vars

---
+++ 02-variable.sh
@@ -0,0 +1,8 @@
+# if we assign a name to a set of data then it is called as variable.
+# syntax: var=data
+x=100
+
+# Access the variable - $var or ${var}
+
+echo value of x - $x
+echo value of x - ${x}
