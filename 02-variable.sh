69004bbe8ecfeaaf301ca44dd8d27c0104551fd5
windows <veeranki2014@gmail.com>
2026-01-26 13:01:47 -0600

    roboshop code dry

--- 02-variable.sh
+++ 02-variable.sh
@@ -6,3 +6,15 @@ x=100
 
 echo value of x - $x
 echo value of x - ${x}
+
+
+# Input from user can be done 3 ways.
+# 1.  BEFORE SCRIPT
+# 2.  RIGHT HAND SIDE
+# 3.  LEFT HAND SIDE
+
+# BEFORE SCRIPT - ENVIRONMENT VARIABLES
+# LHS
+# RHS  - SPECIAL VARIABLES.($1, $1, $@, $*)
+
+echo Env variable - a_env - ${a_env}
