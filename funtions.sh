8faf2ed732ef6b5473e3e34d24bab30d2ebe25f7
windows <veeranki2014@gmail.com>
2026-01-25 23:37:47 -0600

    vars

---
+++ funtions.sh
@@ -0,0 +1,14 @@
+# if we assign a name to a set of commands then it is a function.
+#syntax
+# function function_name() {
+# commands;
+# }
+
+## Sample
+SAMPLE(){
+  echo Sample Function.
+}
+
+## Call a function.
+## function name.
+sample
